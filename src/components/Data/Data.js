29b1794762bd6bb674da2903c31f948d3695e6e9
saisuman-experiments <saisumanpromo@gmail.com>
2025-03-12 22:15:55 +0530

    Initial commit2

---
+++ src/components/Data/Data.js
@@ -0,0 +1,16 @@
+export const data=[
+    {
+        id:1,
+        tpc:"Testing",
+        startDate:"2025-03-11",
+        firstRevision:2,
+        secondRevision:4
+    },
+    {
+        id:1,
+        tpc:"Testing2",
+        startDate:"2025-02-25",
+        firstRevision:2,
+        secondRevision:4
+    },
+]
